cbdb5f5a48bee821b827eea6caad99f320a62338
Murtomaasortaja <daniel.ahrari@protonmail.com>
2023-10-11 21:46:19 +0300

    Add project files.

---
+++ Mangler.cs
@@ -0,0 +1,48 @@
+﻿using CommandLine;
+using System.Reflection;
+
+namespace ManPageMangler
+{
+    public static class Mangler
+    {
+        public static ClassManual[] GetManuals(string assemblyName)
+        {
+            return Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => t.Namespace == assemblyName)
+                .Where(t => t.GetProperties().Any(p => p.GetCustomAttribute<OptionAttribute>() != null))
+                .Select(GetClassManual)
+                .ToArray();
+        }
+
+        private static ClassManual GetClassManual(Type classType)
+        {
+            var options = classType
+                .GetProperties()
+                .Where(p => p.GetCustomAttribute<OptionAttribute>() != null)
+                .Select(p => (p, p.GetCustomAttribute<OptionAttribute>()!))
+                .Select(p => new OptionManual(p.Item1.Name, p.Item2.ShortName, p.Item2.LongName, p.Item2.HelpText))
+                .ToArray();
+
+            var verb = classType.GetCustomAttribute<VerbAttribute>();
+            return new ClassManual(classType.Name, options, verb?.Name, verb?.HelpText);
+        }
+
+        private static string[] GetManualLines(ClassManual classMan)
+        {
+            var result = new List<string>
+            {
+                classMan.VerbName ?? classMan.ClassName,
+                "",
+                classMan.HelpText ?? "No help text provided.",
+                ""
+            };
+            // todo
+            return result;
+        }
+
+        public record ClassManual(string ClassName, OptionManual[] Options, string? VerbName, string? HelpText);
+        public record OptionManual(string PropertyName, string? ShortName, string? LongName, string HelpText);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ManPageMangler
+
+This thing scrapes HelpTexts and stuff from classes decorated with VerbAttribute and with properties decorated with OptionAttribute.
